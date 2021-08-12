e0e1f6f0629789f49b90c28bc4e80df0c7d27161
cemalokten <cemalokten@gmail.com>
2021-08-12 14:22:23 +0100

    Deployed

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 'use strict';
 
+/* ===============================================================
+ * Cemal Okten Portfolio
+ * Author:  Cemal Okten
+ * Language: Javascript
+ * 01 - Variable declarations
+ * 02 - Smooth Scroll
+ * 03 - Colour picker
+ * 04 - Time screensaver
+ * 05 - Image hover
+================================================================== */
+
+// 01 - Variable declarations
+
 const body = document.querySelector('body');
-
 const spans = document.getElementsByTagName('span');
-
 const screensaverContainer = document.querySelector('.screensaver--container');
 const screensaverClock = document.querySelector('.screensaver--clock');
-
 const main = document.getElementsByTagName('main');
-
 const imgContainer = document.querySelectorAll('.img--container');
-
 const pageTitle = document.querySelector('title');
-
 const arr = Array.from(document.getElementsByTagName('section'));
+
+const colourNames = ['rgb(0, 0, 255)', 'rgb(255, 0, 0)'];
+
+//  02 - Smooth Scroll
 
 arr.forEach((section, index) => {
   section.addEventListener('click', function (e) {
-    if (index < 4) {
-      arr[index + 1].scrollIntoView({ behavior: 'smooth' });
-    } else {
-      arr[0].scrollIntoView({ behavior: 'smooth' });
+    if (e.target.localName == 'p') {
+      console.log();
+      if (index < 4) {
+        arr[index + 1].scrollIntoView({ behavior: 'smooth' });
+      } else {
+        arr[0].scrollIntoView({ behavior: 'smooth' });
+      }
     }
   });
 });
 
-// Colours
-
-const colourNames = ['rgb(0, 0, 255)', 'rgb(255, 0, 0)'];
+//  03 - Colour picker
 
 let i = 0;
 
@@ -73,7 +85,6 @@ function timeNow() {
 const getTime = function () {
   setInterval(() => {
     screensaverClock.textContent = timeNow();
-    screensaverClock.style.borderColor = randomColour();
   }, 1000);
 };
 
@@ -87,7 +98,7 @@ function checkIfIdle() {
   idleTime += 1000;
   if (idleTime >= 12000) {
     showScreensaver();
-    pageTitle.textContent = `⌚ ${timeNow()} ⌚`;
+    pageTitle.textContent = `${timeNow()}`;
   }
 }
 
@@ -96,7 +107,7 @@ function resetIdleTime() {
   idleTime = 0;
   hideScreensaver();
   main[0].classList.remove('blur');
-  pageTitle.textContent = `😊`;
+  pageTitle.textContent = `Cemal Okten`;
 }
 
 // Execute checkIfIdle every 1 sec
@@ -107,7 +118,7 @@ document.addEventListener('mousemove', resetIdleTime);
 document.addEventListener('scroll', resetIdleTime);
 document.addEventListener('keydown', resetIdleTime);
 
-// 07 - Image hover
+// 05 - Image hover
 
 function mouseImgOver(e) {
   if (e.target.dataset.id != undefined) {
